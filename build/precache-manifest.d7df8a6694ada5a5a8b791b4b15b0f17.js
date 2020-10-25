2eff3f219307badacd5b72786a98f3dcfaf5fec3
Icesnowliu1128 <3194529680@qq.com>
2020-10-25 12:27:32 +0800

    react-renwu3

---
+++ build/precache-manifest.d7df8a6694ada5a5a8b791b4b15b0f17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd5a26b5be45706e3e56a7087745c90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb5345fd43b71c8525ad",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "254105b44d779aff391c",
+    "url": "./static/css/main.afb88211.chunk.css"
+  },
+  {
+    "revision": "cb5345fd43b71c8525ad",
+    "url": "./static/js/2.6ace3ade.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.6ace3ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254105b44d779aff391c",
+    "url": "./static/js/main.4b72b297.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
